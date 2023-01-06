0b46e1cd8c0dac5381883bf494f93b37803af947
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-01-06 14:35:31 -0500

    fixed errors in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,9 @@ drop database if exists employee_tracker;
 create database employee_tracker;
 use employee_tracker;
 
-create table employee (
+create table department (
     id int not null AUTO_INCREMENT primary key,
-    first_name varchar(30) not null,
-    last_name varchar(30) not null,
-    role_id int not null,
-    manager_id int
+    name varchar(30) not null
 );
 
 create table roles (
@@ -15,11 +12,14 @@ create table roles (
     title varchar(30) not null,
     salary decimal(10) not null,
     department_id int not null,
-    foreign key (id) references employee(role_id)
+    foreign key (department_id) references department(id)
 );
 
-create table department (
+create table employee (
     id int not null AUTO_INCREMENT primary key,
-    name varchar(30) not null,
-    foreign key (id) references roles(department_id)
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int not null,
+    manager_id int,
+    foreign key (role_id) references roles(id)
 );
